a34b5ae16ad0510beb6d349dc930130dd326b7e5
Patrick <patrick.doberiner@gmail.com>
2022-12-27 07:10:22 +0100

    Add poems.js to contain poems object

---
+++ src/poems.js
@@ -0,0 +1,107 @@
+export default [
+    {
+      title:'BONNE ANNÉE',
+      text:`
+        Je tiens à vous souhaiter une bonne année,
+        Qui vaudra beaucoup mieux que les années du passé.
+        Car le Seigneur entend mes prières quotidiennes,
+        Dans lesquelles je n’arrête pas de prier pour qu’il vous protège.
+
+        Que 2023 pour vous soit une année de paix,
+        De joie, d’abondance, de santé.
+        De miracles, de réussite, de bénédiction totale,
+        De merveilles, de prospérité, et d’amour.
+
+        Mon souhait est de vous voir heureux,
+        Danser, célébrer et le succèss chaque jours.
+        Alors je ne vous souhaite que le meilleur, 
+        Bonne année 2023,
+      `,
+      author:'Votre Nom',
+      category:'BA'
+    },
+    {
+      title:'BONNE ANNÉE CHERI',
+      text:`
+      Mon chéri.
+      Nous voici devant une année nouvelle.
+      Le Créateur nous a épargnés des pires dangers de 2020,
+      Tout y est passé :  pandémies, massacres et autres calamités…
+      Enfin !  Nous voici en plein en 2021.
+      
+      Occasion- s’il en fût-chéri, de te souhaiter le meilleur,
+      Afin qu’aux pesanteurs du passé,
+      Réponde une année riche en ouvertures, percées et opportunités diverses.
+      Qui, assurément, fermenteront dans notre relation,
+      Un amour des plus indicibles. 
+      
+      Une année où je ne te verrai pas pleurer et souffrir,
+      Mais plutôt celle où tu crierais de joie et de bonheur.
+      Que 2021 se confirme pour toi une année de paix,
+      D’émerveillement et de consolidation de nos liens d’amour.
+      
+      Je te souhaite une merveilleuse année 2021, chéri.
+      Que Dieu protège notre couple des tempêtes dévastatrices du siècle présent.
+      
+      Je t’aime chéri💖
+      `,
+      author:'Votre Nom',
+      category:'BA'
+    },
+    {
+      title:'BONNE ANNÉE CHERS PARENTS',
+      text:`
+        Mon cher papa,
+        Ma chère maman.
+        C’est de la part de votre enfant,
+        Cet œuvre que vous avez merveilleusement crée.
+
+        Je tiens à vous souhaiter une bonne année,
+        Qui vaudra beaucoup mieux que les années du passé.
+        Car le seigneur entend mes prières quotidiennes,
+        Dans lesquelles je n’arrête pas de prier pour qu’il vous protège.
+
+        Que 2021 pour vous soit une année de paix,
+        De joie, d’abondance, de santé.
+        De miracles, de réussite, de bénédiction totale pour vous,
+        De merveilles, de prospérité, et surtout d’amour entres vous deux.
+
+        Ma vie est belle depuis ma naissance auprès de vous,
+        Je n’ai jamais manqué du nécessaire.
+        Car j’ai deux anges qui me guident sans cesse,
+        Car j’ai Papa et Maman dans ma vie, 
+        Et ils sont toujours là pour prendre soin de moi.
+        Je vous souhaite une merveilleuse année 2021, papa et maman.
+        Merci d’être toujours là pour me guider, me protéger et me couvrir,
+        Je vous aime tellement.
+      `,
+      author:'Votre Nom',
+      category:'BA'
+    },
+    {
+      title:'Happy New Year',
+      text:`
+        Today, Dear
+        We are in front of a new year.
+        God spared us from the worst dangers,
+        As well as the heaviest tears.
+        
+        So here is a new year,
+        Opportunity for me to wish you the best.
+        So that compared to the years of the past,
+        This year should be rich in openings and breakthroughs.
+                          
+        May 2023 be a year of peace for you,
+        A year of success, happiness.
+        May the tears of your eyes never be of sadness,
+        But rather tears of extreme joy.
+        
+        I wish you a wonderful year 2023.
+        May God remove storms from your path.
+      `,
+      author:'Your Name',
+      category:'HNY'
+    },
+  ];
+//   export default menu;
+  
